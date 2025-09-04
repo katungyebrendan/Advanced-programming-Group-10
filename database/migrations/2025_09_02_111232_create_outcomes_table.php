fb51c503c97f757e427be1ed029643c7fadedd5b
katungye <katsbrendan@gmail.com>
2025-09-04 15:05:00 +0300

    project

--- database/migrations/2025_09_02_111232_create_outcomes_table.php
+++ database/migrations/2025_09_02_111232_create_outcomes_table.php
@@ -4,22 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('outcomes', function (Blueprint $table) {
-            $table->id();
+            $table->id('outcome_id');
+
+            $table->foreignId('project_id')
+                  ->constrained('projects', 'project_id')
+                  ->onDelete('cascade');
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('artifact_link')->nullable();
+            $table->string('outcome_type');
+            $table->string('quality_certification')->nullable();
+            $table->string('commercialization_status')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('outcomes');
